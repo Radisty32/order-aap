daa8b6b9bccca7d9a28113d0316eea878c99ce11
Addis Rofik Erlangga <rofikaddis@gmail.com>
2025-06-16 22:43:30 +0700

    membuat class food di file food.dart

---
+++ lib/models/food.dart
@@ -0,0 +1,9 @@
+class Food {
+  final String id;
+  final String name;
+  final double price;
+  final String imageUrl;
+  final String description;
+
+  Food({required this.id, required this.name, required this.price, required this.imageUrl, required this.description});
+}

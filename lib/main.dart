49b168689f47f5803059b27ddfcee2b9bbaad914
Addis Rofik Erlangga <rofikaddis@gmail.com>
2025-06-16 21:53:42 +0700

    membuat classnya

--- lib/main.dart
+++ lib/main.dart
@@ -2,3 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'pages/home_page.dart';
 import 'providers/cart_provider.dart';
+
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CartProvider(),
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Food Order App',
+      theme: ThemeData(primarySwatch: Colors.orange),
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}

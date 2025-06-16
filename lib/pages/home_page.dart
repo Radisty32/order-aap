f1f353aee3c7a712b367b75a51d675aee6276575
Addis Rofik Erlangga <rofikaddis@gmail.com>
2025-06-17 00:45:34 +0700

    - Menambahkan daftar makanan awal ke HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,3 +3,20 @@ import '../models/food.dart';
 import '../widgets/food_tile.dart';
 import 'cart_page.dart';
 
+class HomePage extends StatelessWidget {
+  final List<Food> foods = [
+    Food(
+      id: '1',
+      name: 'Burger',
+      price: 25000,
+      imageUrl: 'https://i.imgur.com/5Aqgz7o.png',
+      description: 'Burger lezat dengan keju dan daging sapi.',
+    ),
+    Food(
+      id: '2',
+      name: 'Pizza',
+      price: 45000,
+      imageUrl: 'https://i.imgur.com/eP4JGJN.png',
+      description: 'Pizza dengan topping keju dan pepperoni.',
+    ),
+  ];

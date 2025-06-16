99721ed2800e65fa07829595aacb7117a021843b
Addis Rofik Erlangga <rofikaddis@gmail.com>
2025-06-17 03:57:53 +0700

    - Membuat widget FoodTile untuk menampilkan informasi makanan

--- lib/widgets/food_tile.dart
+++ lib/widgets/food_tile.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/material.dart';
 import '../models/food.dart';
 import '../pages/food_detail_page.dart';
+
+class FoodTile extends StatelessWidget {
+  final Food food;
+  const FoodTile({required this.food});

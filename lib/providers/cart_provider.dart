27179dcc2451d736c11cae25a5c4f6f7809c4184
Addis Rofik Erlangga <rofikaddis@gmail.com>
2025-06-16 23:16:04 +0700

    membuat class untuk list food

--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -1,2 +1,12 @@
 import 'C:flutter/foundation.dart';
 import '../models/food.dart';
+
+class CartProvider with ChangeNotifier {
+  final List<Food> _cartItems = [];
+
+  List<Food> get items => _cartItems;
+
+  void addToCart(Food food) {
+    _cartItems.add(food);
+    notifyListeners();
+  }

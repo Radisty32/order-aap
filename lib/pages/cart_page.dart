27b17c15d305f39b9ee3284abf2f3bd9c90bc116
Addis Rofik Erlangga <rofikaddis@gmail.com>
2025-06-20 10:49:04 +0700

    menambahkan  fungsionalitas hapus item dari keranjang

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -2,3 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/cart_provider.dart';
 
+
+class CartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final cart = Provider.of<CartProvider>(context);
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Keranjang')),
+      body: cart.items.isEmpty
+          ? Center(child: Text('Keranjang kosong'))
+          : ListView.builder(
+              itemCount: cart.items.length,
+              itemBuilder: (context, index) {
+                final item = cart.items[index];
+                return ListTile(
+                  title: Text(item.name),
+                  subtitle: Text('Rp${item.price.toStringAsFixed(0)}'),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () => cart.removeFromCart(item),
+                  ),
+                );
+              },
+            ),
